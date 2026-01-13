c7e95a14c9d488fc16490f0c9a5aaba45b4233b6
Hevanafa <dhevansafa4@gmail.com>
2026-01-13 11:23:20 +0700

    added VGA unit

--- BMFontEditor/main.pas
+++ BMFontEditor/main.pas
@@ -7,7 +7,9 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls,
-  Graphics, Dialogs, StdCtrls, ExtCtrls;
+  Graphics, Dialogs, StdCtrls, ExtCtrls,
+  { Shared Posit-92 units }
+  VGA;
 
 type
 
@@ -35,14 +37,26 @@ implementation
 procedure TForm1.FormCreate(Sender: TObject);
 var
   bmp: TBitmap;
+  destPtr: PByte;
+  srcPtr: PByteArray;
+  bufferSize: integer;
 begin
+  initBuffer;
+  cls($FF6495ED);
+
+  srcPtr := getSurfacePtr;
+  bufferSize := vgaWidth * vgaHeight * 4;
+
   bmp := Image1.Picture.Bitmap;
+  destPtr := PByte(bmp.RawImage.Data);
   bmp.PixelFormat := pf32bit;  { RGBA }
   bmp.SetSize(320, 200);
 
   { bmp.canvas.Brush.Color := $ED9564; }  { BGR format of cornflower blue }
-  bmp.canvas.brush.Color := RGBToColor($64, $95, $ED);
-  bmp.canvas.FillRect(0, 0, 320, 200);
+  { bmp.canvas.brush.Color := RGBToColor($64, $95, $ED);
+  bmp.canvas.FillRect(0, 0, 320, 200); }
+
+  move(srcPtr, destPtr, bufferSize);
 
   image1.Invalidate
 end;
