d84fbdf67d112cbbf5220d64274fa6ccf291cf0f
Hevanafa <dhevansafa4@gmail.com>
2026-01-13 11:27:36 +0700

    added copy buffer

--- BMFontEditor/main.pas
+++ BMFontEditor/main.pas
@@ -44,20 +44,23 @@ begin
   initBuffer;
   cls($FF6495ED);
 
-  srcPtr := getSurfacePtr;
-  bufferSize := vgaWidth * vgaHeight * 4;
-
   bmp := Image1.Picture.Bitmap;
-  destPtr := PByte(bmp.RawImage.Data);
   bmp.PixelFormat := pf32bit;  { RGBA }
-  bmp.SetSize(320, 200);
+  bmp.SetSize(vgaWidth, vgaHeight);
+
+  bmp.BeginUpdate;  { Important: allocate TBitmap buffer }
+
+  srcPtr := getSurfacePtr;
+  destPtr := PByte(bmp.RawImage.Data);
+  bufferSize := vgaWidth * vgaHeight * 4;
 
   { bmp.canvas.Brush.Color := $ED9564; }  { BGR format of cornflower blue }
   { bmp.canvas.brush.Color := RGBToColor($64, $95, $ED);
   bmp.canvas.FillRect(0, 0, 320, 200); }
 
-  move(srcPtr, destPtr, bufferSize);
+  move(srcPtr^, destPtr^, bufferSize);
 
+  bmp.EndUpdate;
   image1.Invalidate
 end;
 
