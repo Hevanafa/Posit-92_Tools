91fb8b5fff2fc85e6b1de8789364cce8c99046d3
Hevanafa <dhevansafa4@gmail.com>
2026-01-13 11:48:08 +0700

    added shared units from Posit-92 Wasm

--- BMFontEditor/main.pas
+++ BMFontEditor/main.pas
@@ -1,7 +1,7 @@
 unit Main;
 
 {$Mode ObjFPC}
-{$H+}
+{$H-}
 
 interface
 
@@ -9,15 +9,16 @@ uses
   Classes, SysUtils, Forms, Controls,
   Graphics, Dialogs, StdCtrls, ExtCtrls,
   { Shared Posit-92 units }
-  VGA;
+  BMFont, Conv, Logger, UStrings, VGA;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Image1: TImage;
-    Memo1: TMemo;
+    btnRender: TButton;
+    imgCanvas: TImage;
+    memoInput: TMemo;
     procedure FormCreate(Sender: TObject);
   private
 
@@ -34,6 +35,111 @@ implementation
 
 { TForm1 }
 
+{ 32 to 126: 0 to 94 }
+procedure loadBMFont(const filename: string; var font: TBMFont; var fontGlyphs: array of TBMFontGlyph);
+var
+  f: text;
+  txtLine: string;
+  a: word;
+  pairs: array[0..9] of string;
+  pair: array[0..1] of string;
+  k, v: string;
+  tempGlyph: TBMFontGlyph;
+  glyphCount: word;
+begin
+  assign(f, filename);
+  {$I-} reset(f); {$I+}
+
+  if IOResult <> 0 then begin
+    writeLog('Failed to open BMFont file: ' + filename);
+    exit
+  end;
+
+  glyphCount := 0;
+  while not eof(f) do begin
+    readln(f, txtLine);
+
+    if startsWith(txtLine, 'info') then begin
+      split(txtLine, ' ', pairs);
+
+      for a:=0 to high(pairs) do begin
+        split(pairs[a], '=', pair);
+        k := pair[0]; v := pair[1];
+        if k = 'face' then
+          font.face := replaceAll(v, '"', '');
+      end;
+
+      { writeLog('font.face:' + font.face) }
+
+    end else if startsWith(txtLine, 'common') then begin
+      split(txtLine, ' ', pairs);
+
+      for a:=0 to high(pairs) do begin
+        split(pairs[a], '=', pair);
+        k := pair[0]; v := pair[1];
+        if k = 'lineHeight' then
+          font.lineHeight := parseInt(v);
+      end;
+
+    end else if startsWith(txtLine, 'page') then begin
+      split(txtLine, ' ', pairs);
+
+      for a:=0 to high(pairs) do begin
+        split(pairs[a], '=', pair);
+        k := pair[0]; v := pair[1];
+        if k = 'file' then
+          font.filename := replaceAll(v, '"', '');
+      end;
+
+    end else if startsWith(txtLine, 'char') and not startsWith(txtLine, 'chars') then begin
+      while contains(txtLine, '  ') do
+        txtLine := replaceAll(txtLine, '  ', ' ');
+
+      { Parse the whole nine first, then copy the record to the list of font glyphs }
+      split(txtLine, ' ', pairs);
+
+      for a:=0 to high(pairs) do begin
+        split(pairs[a], '=', pair);
+        k := pair[0]; v := pair[1];
+
+        { case-of can't be used with strings in Mode TP }
+        if k = 'id' then
+          tempGlyph.id := parseInt(v)
+        else if k = 'x' then
+          tempGlyph.x := parseInt(v)
+        else if k = 'y' then
+          tempGlyph.y := parseInt(v)
+        else if k = 'width' then
+          tempGlyph.width := parseInt(v)
+        else if k = 'height' then
+          tempGlyph.height := parseInt(v)
+        else if k = 'xoffset' then
+          tempGlyph.xoffset := parseInt(v)
+        else if k = 'yoffset' then
+          tempGlyph.yoffset := parseInt(v)
+        else if k = 'xadvance' then
+          tempGlyph.xadvance := parseInt(v);
+      end;
+
+      { array of glyphs starts from 0, ends at 94 }
+      { Assuming glyph.id always starts from 32 }
+      if (tempGlyph.id - 32) in [low(fontGlyphs)..high(fontGlyphs)] then begin
+        fontGlyphs[tempGlyph.id - 32] := tempGlyph;
+        inc(glyphCount)
+      end;
+    end;
+  end;
+
+  close(f);
+
+  writeLog('Loaded ' + i32str(glyphCount) + ' glyphs');
+
+  font.imgHandle := loadImage(font.filename);
+
+  writeLog('font.imgHandle');
+  writeLogI32(font.imgHandle);
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 var
   bmp: TBitmap;
@@ -45,7 +151,7 @@ begin
   initBuffer;
   cls($FF6495ED);
 
-  bmp := Image1.Picture.Bitmap;
+  bmp := imgCanvas.Picture.Bitmap;
   bmp.PixelFormat := pf32bit;  { 32-bit BGRA }
   bmp.SetSize(vgaWidth, vgaHeight);
 
@@ -55,9 +161,8 @@ begin
   destPtr := PByte(bmp.RawImage.Data);
   bufferSize := vgaWidth * vgaHeight * 4;
 
-  { move(srcPtr^, destPtr^, bufferSize); }
   a:=0;
-  while a < bufferSize - 1 do begin
+  while a < bufferSize do begin
     destPtr[0] := srcPtr[2];
     destPtr[1] := srcPtr[1];
     destPtr[2] := srcPtr[0];
@@ -69,7 +174,7 @@ begin
   end;
 
   bmp.EndUpdate;
-  image1.Invalidate
+  imgCanvas.Invalidate
 end;
 
 end.
